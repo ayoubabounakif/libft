5951c483f8c5da051d80c692a6869347a8163568
Ayoub Abounakif <aabounak@e0r3p1.1337.ma>
2021-07-09 15:53:06 +0100

    [OK]

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,37 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aabounak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/14 19:19:02 by aabounak          #+#    #+#             */
-/*   Updated: 2019/10/15 20:02:50 by aabounak         ###   ########.fr       */
+/*   Created: 2021/07/09 15:51:37 by aabounak          #+#    #+#             */
+/*   Updated: 2021/07/09 15:52:41 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+# include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int		size;
-	int		temp;
-
-	size = 1;
-	if (n == -2147483648)
-		write(fd, "-2147483648", 11);
-	if (n < 0 && n != -2147483648)
-	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	temp = n;
-	while (temp > 0)
-	{
-		temp /= 10;
-		size *= 10;
-	}
-	temp = n;
-	while (size && n != -2147483648)
-	{
-		ft_putchar_fd((temp / size) + '0', fd);
-		temp %= size;
-		size /= 10;
-	}
+	if (n / 10)
+			ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd(n % 10 + '0', fd);
+	return ;
 }
